4375d0450225f3d60be182e9871b5bccc04c15dd
Amy Liffey <amy.liffey@hevs.ch>
2024-04-03 16:42:58 +0200

    feat: simple test template

---
+++ test/SimpleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+	
+	public Context context;
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
